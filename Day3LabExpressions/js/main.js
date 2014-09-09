46cc0c39b3eede889709dc69e907f2c774d2f1bb
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-08 23:37:04 -0400

    Part 2
    equation

--- Day3LabExpressions/js/main.js
+++ Day3LabExpressions/js/main.js
@@ -1,6 +1,7 @@
 //Debbie Walker, September 8, 2014, Web Programming Fundamentals - Lecture 00, Day 3 Lab Expressions
 
 //Part 1
+//Collection of data:
 // Figure out what the following variables will be for a pizza party.
 var slices=8;
 var people=6;
@@ -9,7 +10,12 @@ var pizzas=4;
 //How many slices will each person get?
 var person=(slices * pizzas /people)
 
+//Browser document post.
 console.log("Each person will get " + person)
 
 
 //Part 2
+//How much left over pizza will there be if all the pizza slice where whole?
+//Equation: remainder of whole pizza = .333 * people (6) = x
+console.log(.333 * people)
+
