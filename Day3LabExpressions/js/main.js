29e3e454e8f41110776a9e20b7b3e394057187d1
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-09 19:34:21 -0400

    Notes WIP

--- Day3LabExpressions/js/main.js
+++ Day3LabExpressions/js/main.js
@@ -5,14 +5,34 @@
 // Javascript: Frontend programming language (or set of instructions); it's primary function is to control elements in a browser.
         // Frontend: Means client side which lives on a computer as opposed to a server (it is referred to as a loosely type language because its variables can contain abstract data).
 
-//Expressions: "Building blocks for Java program...using values, variables, operators and method calls." Reference/source: http://java.about.com/od/e/g/expression.htm
+//Expressions: "Building blocks for Java program...using 1. values, 2. variables, 3. operators and 4. method calls." Reference/source: http://java.about.com/od/e/g/expression.htm
         //Expressions such as slicesOfPizza, or minutesInHour give evaluation (http://dictionary.reference.com/browse/evaluate?s=t) to a specific value.
 
-            //Value:
-            //Variables: Are a container of data containing a  - for example: var score=0
-            //Operators: Are symbols that assign values to variables. Reverence/source: http://www.w3schools.com/js/js_operators.asp
+            //1. Value: Reference/source: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Values,_variables,_and_literals?redirectlocale=en-US&redirectslug=JavaScript%2FGuide%2FValues%2C_variables%2C_and_literals
+                      //Reverence/source: http://www.w3schools.com/js/js_variables.asp
+                //1a. Type of Value	        Examples of typed values
+                    //Numbers	            42, 3.14159
+                    //Logical               (Boolean)	true / false
+                    //Strings	            "Howdy" or "20" in this case the number is a string because of the "" which gives it a different outcome
+                    //null	                a special keyword denoting a null value; null is also a primitive value. Because JavaScript is case-sensitive, null is not the same as Null, NULL, or any other variant (USE CAMO CASE,
+                    //undefined	            a "In computer programs, variables are often declared without a value. The value can be something that has to be calculated, or something that will be provided later, like user input. Variable declared without a value will have the value undefined, for example: var carName;"
+
+            //2. Variables: Are a container of data containing a  - for example: var score=0; or you can have one statement with many variables for example: var lastName = "Doe", age = 30, job = "carpenter";
+                    // Variable names must begin with a letter
+                    // Variable names can also begin with $ and _ (but do not use it)
+                    // Variable names are case sensitive (y and Y are different variables)
+                    // Reserved words (like JavaScript keywords) cannot be used as variable names
+
+            //3. Operators: Are symbols that assign values to variables. Reverence/source: http://www.w3schools.com/js/js_operators.asp
                         //Assigned operators: The most common is the = symbol "is used to assign values to JavaScript variables." It is preceded by an operand/operation on its left and a value on its right for example: slices= 8)
-            //Method calls:
+
+            //4. Method calls:
+
+// Declaring: Creating a variable in JavaScript is called "declaring" a variable
+
+
+//variables, constants, and literals
+
 
 // Forms:
 // Form Objects:
@@ -23,9 +43,6 @@
 // Object Collection:
 
 // Document Object:
-
-// Declarations:
-
 
 
 // Operators:
