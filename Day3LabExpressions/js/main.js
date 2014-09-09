8fa453071f450da71fb53bbcac74424122001923
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-08 22:48:25 -0400

    Problem 1
    solving

--- Day3LabExpressions/js/main.js
+++ Day3LabExpressions/js/main.js
@@ -1,1 +1,19 @@
 //Debbie Walker, September 8, 2014, Web Programming Fundamentals - Lecture 00, Day 3 Lab Expressions
+
+//Part 1
+// How many pizza slices will everyone get and how much will be left over fot the dog?
+var slices=8;
+var people=6;
+var pizzas=4;
+
+//How many slices will each person get?
+var person=(slices * pizzas /people)
+
+//What will the left over of pizza be?
+console.log(person)
+
+
+// How much did every person eat?
+console.log ()
+
+//Part 2
