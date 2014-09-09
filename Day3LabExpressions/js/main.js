e00e0ff8e1b17d8fa758b574f96b4f3f582fb50b
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-09 16:00:56 -0400

    Adding Vocabulary Notes

--- Day3LabExpressions/js/main.js
+++ Day3LabExpressions/js/main.js
@@ -1,13 +1,31 @@
 //Debbie Walker, September 8, 2014, Web Programming Fundamentals - Lecture 00, Day 3 Lab Expressions
 
-//Part 1
-//Collection of data:
-// Figure out what the following variables will be for a pizza party.
+//Personal pre-assignment vocabulary in alphabetical order for me to better understand:
+
+//Operators: Are symbols that assign values to variables.
+     //Assigned operators: The most common is the = symbol "is used to assign values to JavaScript variables" (it is preceded by an operand/operation on its left and a value on its right - it might look like this slices= 8)
+
+
+//Expressions: "Building blocks for Java program...using values, variables, operators and method calls." Reference/source: http://java.about.com/od/e/g/expression.htm
+    //Expressions such as slicesOfPizza, or minutesInHour give evaluation (http://dictionary.reference.com/browse/evaluate?s=t) to a specific value
+
+// Javascript: Frontend programming (set of instructions) language, it's primary function is to control elements in a browser.
+    // Frontend software = client side which lives on a computer as opposed to a server (it is referred to as a loosely type language because its variables can contain abstract data).
+    //An array of primary browsers is ["Google Chrome + Google Chrome Canary", "Firefox + Firebug", + "Safari + Safari Webkit"].
+    //Examples of the 10 data types:
+        //1. var score= 5 - this is an example of a number data type, which is a number shown by itself
+        //2. var score= "Bob" - this is an example of a string, which is set off by quotation marks
+        //3. var score= true or false - this is an example of of a boolean
+
+
+
+//Part 1 | Collection of data:
+// How many pizza slices will everyone at a pizza party get, if there are...
 var slices=8;
 var people=6;
 var pizzas=4;
 
-//How many slices will each person get?
+//The equation for the
 var person=(slices * pizzas /people);
 
 //Browser document post.
@@ -24,7 +42,7 @@ console.log("Sparky received " + leftOver + " slice of pizza.");
 //Part 3
 //Budgeting groceries.
 
-var total=750.00;
+var total=550.00;
 var average=[total / 5];
 
 console.log("I have spent $" + total.toFixed(2) + " on groceries over 5 weeks. " + "That is an average of $" + average  + ".00 per week.");
@@ -43,6 +61,6 @@ var withTax=price + tax;
 var withoutTax=price;
 
 var discountWithoutTax=price * (1- discount);
-var discountWithTax=discountWithoutTax * (1- tax)
+var discountWithTax=discountWithoutTax * (1+ tax)
 
-console.log("The " + description + " was originally $" + price + ".00, but after a " + discount + " discount was applied, it is now only" + " $" + discountWithoutTax + " without tax and $" + discountWithTax);
+console.log("The " + description + " was originally $" + price + ".00, but after a " + discount + " discount was applied, it is now only" + " $" + discountWithoutTax + " without tax and $" + discountWithTax + " after tax.");
