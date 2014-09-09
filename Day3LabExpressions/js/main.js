a0cc3a2f49d9e17fd8d504751a9faa7144731c56
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-09 14:25:06 -0400

    WIP
    part 4

--- Day3LabExpressions/js/main.js
+++ Day3LabExpressions/js/main.js
@@ -8,18 +8,18 @@ var people=6;
 var pizzas=4;
 
 //How many slices will each person get?
-var person=(slices * pizzas /people)
+var person=(slices * pizzas /people);
 
 //Browser document post.
-console.log("Each person will get " + person + " slices of pizza.")
+console.log("Each person will get " + person + " slices of pizza.");
 
 
 //Part 2
 //How much left over pizza will there be if all the pizza slice where whole?
 //Equation: remainder of whole pizza = .333 * people (6) = x
-var leftOver= (slices * pizzas %people)
+var leftOver= (slices * pizzas %people);
 
-console.log("Sparky received " + leftOver + " slice of pizza.")
+console.log("Sparky received " + leftOver + " slice of pizza.");
 
 //Part 3
 //Budgeting groceries.
@@ -27,19 +27,21 @@ console.log("Sparky received " + leftOver + " slice of pizza.")
 var total=750.00;
 var average=[total / 5];
 
-console.log("I have spent $" + total.toFixed(2) + " on groceries over 5 weeks. " + "That is an average of $" + average  + ".00 per week.")
+console.log("I have spent $" + total.toFixed(2) + " on groceries over 5 weeks. " + "That is an average of $" + average  + ".00 per week.");
 
 //Part 4
 
 
 //Your X was originally $X, but after a X% discount, it is now $X without tax, and $X with tax.
 
-var price=10
-var discount=.05
-var item=shoe
-var tax=.03
+var price=10;
+var discount=.05;
+var description="pair of shoes";
+var tax=.03;
 
-var itemTotal= price + tax
-var item= price
+var withTax= price + tax;
+var withoutTax= price;
 
-console.log("The " + item + " was originally $" + price + ".00, but after a" + discount + ".00 , it is now " + price * 1 - discount
+var discountTotal=price * (1- discount);
+
+console.log("The " + description + " were originally $" + price + ".00, but after a " + discount + " discount was applied, it is now only");
