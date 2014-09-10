05215603c06625003585ae7a89607ffc5db77d49
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-10 15:52:32 -0400

    Day4Expressions_Wacky
    File and folder setup

---
+++ Day4Expressions_Wacky/js/main.js
@@ -0,0 +1,22 @@
+//Debbie Walker, September 10, 2014, Web Programming Fundamentals - Lecture 00, Day 4 Expressions_Wacky
+
+//Assignment: You will create three separate projects, each a set of files and folders.
+// Each set will be a calculator that collects information from the user with prompts,
+// calculates some sort of useful result with that information and print a result to the console.
+
+
+
+//Industry-specific: This second set should be something related to either your current job or the web design and development industry.
+//A good example of this would be something that calculates how many pixels square an image resolution is based on image dpi, width and height.
+
+
+// Contacts calculation
+// I need to figure .
+// Assume .
+//Given:
+// •
+
+// Result Variable:
+// •
+// •
+// •
