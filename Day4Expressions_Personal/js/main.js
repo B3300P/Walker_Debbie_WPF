b6413b6cde53e1990fa5d732f27c01bb387e9162
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-10 15:29:55 -0400

    Day4Expression_Personal
    WIP

---
+++ Day4Expressions_Personal/js/main.js
@@ -0,0 +1,27 @@
+//Debbie Walker, September 10, 2014, Web Programming Fundamentals - Lecture 00, Day 4 Expressions_Personal
+
+//Assignment: You will create three separate projects, each a set of files and folders.
+// Each set will be a calculator that collects information from the user with prompts,
+// calculates some sort of useful result with that information and print a result to the console.
+
+
+
+//Personal: The first set should be something personal to you. Some examples
+//Code that calculates how many diapers you will need over the course of a year.
+  //  How much paint you would need for a room in your home.
+
+
+// Contacts calculation
+// I need to figure out how many boxes of contacts that I will need to purchase to last me until my next doctor appointment.
+// Assume contacts are monthly disposable and the doctor appointments are scheduled exactly one year apart.
+//Given:
+// • a box can hold up to 10 packages
+
+// Result Variable:
+// • Total package
+// • Total box
+// • Total per eye
+
+
+
+
