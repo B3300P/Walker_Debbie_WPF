343e4fdd67b71b72ec2678c0f33a7ad0abb48732
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-10 18:57:52 -0400

    WIP

--- Day4Practice/js/main.js
+++ Day4Practice/js/main.js
@@ -3,7 +3,22 @@
  */
 
 //create array
-var advengerNames= ["Ironman", "Captain America", "Hulk", "BlackWidow", "Thor", "Parthenon", "Spiderman"];
+var avengerNames= ["Ironman", "Captain America", "Hulk", "Black Widow", "Thor", "Parthenon", "Spiderman"];
 
 //print the array to js console
-console.log(advengerNames);
+console.log(avengerNames);
+
+//access a specific item in array
+console.log(avengerNames[0];
+
+//change an item in the array
+aventerNames[0] = "Black Widow";
+
+//print the array again and by index number
+consle.log(avengerName);
+consle.log(avangerNames[0];
+
+//find out hte length of an array
+consle.log(avengerNames.length);
+
+
