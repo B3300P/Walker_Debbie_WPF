4ff0644f8f1f4e83cd5311d7a937e2ed04985819
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-10 19:20:04 -0400

    WIP

--- Day4Practice/js/Day4PromptPractice.js
+++ Day4Practice/js/Day4PromptPractice.js
@@ -1,6 +1,4 @@
-/**
- * Created by debbiewalker on 9/10/14.
- */
+//Created by debbiewalker on 9/10/14.
 
 //ask user for input
 //prompt();
@@ -12,7 +10,7 @@ alert(name);
 //calculate the are of a rectangle
 
 var width = prompt("What is the width of your room?");
-var height = prompt("What is the length of your room?");
+var height = prompt("What is the height of your room?");
 
 var area = width*height;
 
@@ -23,3 +21,8 @@ var area = width*height;
 console.log(area);
 alert(area);
 
+//
+var areaCode = prompt("What is your area code?");
+var  phoneNumer = prompt("What is your phone number without area code?");
+
+String(areaCode)+ String(phoneNumber);
