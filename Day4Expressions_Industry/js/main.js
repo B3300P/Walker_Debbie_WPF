754ac5744f910bce7d39f6f67a269faae120b95d
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 13:20:30 -0400

    Problem set up

--- Day4Expressions_Industry/js/main.js
+++ Day4Expressions_Industry/js/main.js
@@ -1,17 +1,16 @@
 //Debbie Walker, September 10, 2014, Web Programming Fundamentals - Lecture 00, Day 4 Expressions_Industry-Specific
 
-//Problem: Place problem here?
+//Problem: Setting up a website.
 
-//Question:
-// 1.
-// 2.
-// 3.
-//Answer: colors;
-// 1.
-// 2.
-// 3.
+//Choices:
+// 1. Choose a font.
+// 2. Pick a background color.
+// 3. Choose where you would like your logo to be placed in the header.
+//Answer:
+// 1. Helvetica, Gills Sans, Times Roman, Avant Gard
+// 2. White, Black, Grey, Beige
+// 3. Top left, Center, Top right
 //Comments:
-// 1.
-// 2.
-// 3.
-
+// 1. ____________ is a great font for your website.
+// 2. Your background color will be ____________.
+// 3. Thank you, your logo will be placed in the _________ of the header.
