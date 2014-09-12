400497cf04895fe3c35e0761dec15e45b44e3523
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 13:28:08 -0400

    First Variable

--- Day4Expressions_Industry/js/main.js
+++ Day4Expressions_Industry/js/main.js
@@ -14,3 +14,12 @@
 // 1. ____________ is a great font for your website.
 // 2. Your background color will be ____________.
 // 3. Thank you, your logo will be placed in the _________ of the header.
+
+// Equations needed:
+var font = ["Helvetica", " Gills Sans", " Times Roman", " Avant Gard"];//This is an array, an array contains
+// variables set off by brackets. // Each variable must be enclosed with quote marks and followed by a comma.
+var fontChoice = prompt("Choose a font. " + font);//This is an example of prompt.
+// A prompt includes "prompt()" Inside the prompt is a concatenation that includes a string that tells
+// something plus a variable.
+console.log(fontChoice + ", is a great choice for your website.");//This is a console.log which prints
+//to the browser.
