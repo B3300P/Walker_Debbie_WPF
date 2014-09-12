8cf0e4091d427bb7a5d45a2189d207bcdd8c71c8
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-11 20:10:20 -0400

    Practice problems for Day4 homework

--- reference/js/main.js
+++ reference/js/main.js
@@ -4,6 +4,8 @@ var helloWord = "Hello World!"; // hellowWorld is a variable name, and "Hello Wo
 var bowlArray = ["ball", "shell", helloWord]; // This is an example of an array
 console.log(bowlArray); // This is how to print your information
 
+//Problem: What is the average age of x people?
+
 var Jeff= 28; // Jeff is a name of a variable holding an integer/number (which is a type of value) // There are three types of values; boolean, integer and string
 var Jen=12; // SAME FOR ALL FOUR VARIABLES, they are INTEGERS/numbers so that I'm clear!!!!
 var Mike=29;
@@ -11,9 +13,27 @@ var Debbie=100;
 var groupArray = [Jeff, Jen, Mike, Debbie]; // This is an example of an array; must be in brackets;
 // they are not strings, they are variables
 var average = (Jeff + Jen + Mike + Debbie) / groupArray.length;// the mathematical equation for the problem which is the average age of 4 people is the addition of all four divided by the same number of people
+// groupArray.length this code is used to dived a variable into itself - for example groupArray = 4 and .length divides groupArrays total by that number "4"
 console.log("average: " + average);
 /* Anything between the a forward slash and asterisks and the the asterisks and the forward slash
 *
 *
 * */
+
+
+
+// Problem: What is the total in my piggy bank.
+
+//Given:
+//72 $1
+//12 $5
+//27 $10
+
+// Variable:
+var oneDollars = 72;
+var fiveDollars = 12;
+var tenDollars = 27;
+var total = oneDollars + fiveDollars + tenDollars;
+
+console.log ("The total in my piggy bank is $" + total + ".00.");
 
