08e37ceb98a55ce23e39876bb5883c73283dc139
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-11 21:57:08 -0400

    Practice WIP

--- reference/js/main.js
+++ reference/js/main.js
@@ -1,21 +1,29 @@
-var bowl = "shell";  //var is variable, bowl is the name of the variable, = assignment operator and shell is the value
-var helloWord = "Hello World!"; // hellowWorld is a variable name, and "Hello World!" is a string
+var bowl = "shell";  //var is variable, bowl is the name of the variable, = assignment
+// operator and shell is the value
+var helloWord = "Hello World!"; // hellowWorld is a variable name, and "Hello World!"
+// is a string
 // Declaration bowl = x
 var bowlArray = ["ball", "shell", helloWord]; // This is an example of an array
 console.log(bowlArray); // This is how to print your information
 
 //Problem: What is the average age of x people?
 
-var Jeff= 28; // Jeff is a name of a variable holding an integer/number (which is a type of value) // There are three types of values; boolean, integer and string
-var Jen=12; // SAME FOR ALL FOUR VARIABLES, they are INTEGERS/numbers so that I'm clear!!!!
+var Jeff= 28; // Jeff is a name of a variable holding an integer/number (which is a
+// type of value) // There are three types of values; boolean, integer and string
+var Jen=12; // SAME FOR ALL FOUR VARIABLES, they are INTEGERS/numbers so that I'm
+// clear!!!!
 var Mike=29;
 var Debbie=100;
 var groupArray = [Jeff, Jen, Mike, Debbie]; // This is an example of an array; must be in brackets;
 // they are not strings, they are variables
-var average = (Jeff + Jen + Mike + Debbie) / groupArray.length;// the mathematical equation for the problem which is the average age of 4 people is the addition of all four divided by the same number of people
-// groupArray.length this code is used to dived a variable into itself - for example groupArray = 4 and .length divides groupArrays total by that number "4"
+var average = (Jeff + Jen + Mike + Debbie) / groupArray.length;// the mathematical
+// equation for the problem which is the average age of 4 people is the addition of
+// all four divided by the same number of people
+// groupArray.length this code is used to dived a variable into itself - for example
+// groupArray = 4 and .length divides groupArrays total by that number "4"
 console.log("average: " + average);
-/* Anything between the a forward slash and asterisks and the the asterisks and the forward slash
+/* Anything between the a forward slash and asterisks and the the asterisks and the
+forward slash
 *
 *
 * */
@@ -63,9 +71,11 @@ var day2 = 24
 var labor = designer + developer + projectManager + writer;
 var total = labor;
 // Expression: labor / budget
-var totalHours = total / budget
+var totalHours = Math.round(budget / total);
 
-console.log("The total labor of the team will be " + labor + " hours an hour to develop a website. With a budget of $ " + budget + " the team will be able to work " + totalHours + ".")
+console.log("The total labor of the team will be $" + labor + ".00 an hour to develop" +
+    " a website. With a budget of $" + budget + ".00 the team will be able to work " +
+    totalHours + " hours.")
 
 
 
