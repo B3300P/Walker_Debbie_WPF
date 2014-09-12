ca5fd36cc676197267257cd450c60686289ba0ed
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 10:15:25 -0400

    WIP

--- reference/js/main.js
+++ reference/js/main.js
@@ -78,4 +78,16 @@ console.log("The total labor of the team will be $" + labor + ".00 an hour to de
     totalHours + " hours.")
 
 
+//Problem: What color of bath would you like?
 
+//Question: 1. What's your favorite color?, 2. Would you like to add bubbles? 3. What scent would you like?
+//Answer: colors; 1. pink, yellow, blue, purple
+// 2. yes or no
+// 3. bubble gum, sunshine, ocean, grape soda
+//Comments: 1. RANDOM Lovely color choice., That's my favorite color too., That fits your mood perfectly.
+// 2. yes; Bubbles make an ordinary day extraordinary no; Simple is very zen.
+// 3. Your fragrance will enhance any room you step into.
+
+// Equations needed:
+
+var color 
