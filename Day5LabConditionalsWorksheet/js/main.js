65e8304f000602080f3d52296e8b5de9f4fa6b09
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 19:05:54 -0400

    Practice

--- Day5LabConditionalsWorksheet/js/main.js
+++ Day5LabConditionalsWorksheet/js/main.js
@@ -2,3 +2,32 @@
 
 //Problem: How to enroll at Full Sail.
 
+var oldEnough = true;
+//if the child is old nough, they can ride!!
+if(oldEnough){
+    //if oldEnough is true, then you can ride!
+    //if oldEnough is false, then nothing happens
+   console.log("You can ride the coaster!");
+}
+
+//Relational Expressions
+//if the kid is tall enough, then they can ride
+//must me 48 inches tall or over
+var kidheight = 40;
+// kid's height
+var minheight = 48;
+// min height to rid the coaster
+
+if(kidheight >= minheight){
+    //if the kid is taller than or equal to the min
+    //height they can ride the coaster
+    console.log("You can ride the coaster!!")
+}
+
+//test true and false to see if equation works
+
+
+
+
+
+//Conditional with an expression
