04fb7ed685fc611d1bd61987b98f3b79b0254400
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 14:06:42 -0400

    Problem set up

--- Day4Expressions_Wacky/js/main.js
+++ Day4Expressions_Wacky/js/main.js
@@ -1,18 +1,21 @@
 //Debbie Walker, September 10, 2014, Web Programming Fundamentals - Lecture 00, Day 4 Expressions_Wacky
 
 
-//Problem: 
+//Problem: What will you be for Halloween?
 
 //Choices:
-// 1.
-// 2.
-// 3.
+// 1. Pick your nose.
+// 2. Choose a wig.
+// 3. What do you smell like?
+// 4. You will go as.
 //Answer:
-// 1.
-// 2.
-// 3.
+// 1. clown, snout, trunk, witch
+// 2. punk, bald, short curly, long, bob
+// 3. trash, skunk, perfume, chocolate
+// 4. RANDOM Porky The Pig, Oscar the Grouch, Flying Purple Eater, Wicked Witch of the West
 //Comments:
-// 1.
+// 1. Picking your nose is disgusting, but a _________ nose will suite you just fine.
 // 2.
 // 3.
+// 4. You've gone batty, I'll bet you'll steal the part as a __________ this Halloween.
 
