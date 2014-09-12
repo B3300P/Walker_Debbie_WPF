09ede6d0668c75c6df7c3c1843f1e287fb738767
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-12 14:41:07 -0400

    Variable 3
    in progress

--- Day4Expressions_Wacky/js/main.js
+++ Day4Expressions_Wacky/js/main.js
@@ -16,8 +16,8 @@
 //Comments:
 // 1. Picking your nose is disgusting, but choosing a _________ nose is awesome.
 // 2. It's getting hairy up here, looks like your __________ wig suites your nose.
-// 3. Freakazoid Zombie
-// 4. I'll bet you'll steal the show as a __________ this Halloween.
+// 3. Wow! That _______ smell really completes your costume!
+// 4. I'll bet you'll steal the show as a Freakazoid Zombie this Halloween.
 
 
 // Equations needed:
@@ -26,21 +26,22 @@ var noseChoices = ["clown", " snout", " trunk", " witch"];//This is an array, an
 var nose = prompt("Pick your nose: " + noseChoices);//This is an example of prompt.
 // A prompt includes "prompt()" Inside the prompt is a concatenation that includes a string that tells
 // something plus a variable.
-console.log("Picking your nose is disgusting, but choosing a " + nose + " nose is awesome.");//This is a console.log which prints
-//to the browser.
+console.log("Picking your nose is disgusting, but choosing a " + nose + " nose is awesome.");//This is a console.log
+// which prints to the browser.
 
 var wigChoices = ["punk", " bald", " short curly", " long straight"];//This is an array, an array contains
 // variables set off by brackets. // Each variable must be enclosed with quote marks and followed by a comma.
 var wig = prompt("Choose a wig: " + wigChoices);//This is an example of prompt.
 // A prompt includes "prompt()" Inside the prompt is a concatenation that includes a string that tells
 // something plus a variable.
-console.log("It's getting hairy up in here, looks like your " + wig + " hair choice goes well with your nose choice.");//This is a console.log which prints
+console.log("It's getting hairy up in here, looks like your " + wig + " hair choice goes well with your nose choice.");
+//This is a console.log which prints
 //to the browser.
 
 var scentChoices = ["trash", " skunk", " perfume", " chocolate"];//This is an array, an array contains
 // variables set off by brackets. // Each variable must be enclosed with quote marks and followed by a comma.
-var scent = prompt("Choose a wig: " + wigChoices);//This is an example of prompt.
+var scent = prompt("What will you smell like? " + scentChoices);//This is an example of prompt.
 // A prompt includes "prompt()" Inside the prompt is a concatenation that includes a string that tells
 // something plus a variable.
-console.log("It's getting hairy up in here, looks like your " + wig + " hair choice goes well with your nose choice.");//This is a console.log which prints
-//to the browser.
+console.log("Wow! That " + scent + " smell really completes your costume!");
+//This is a console.log which prints to the browser.
