a16d9f6f8e2fdee0a8c1f0f12cbcba89d073ad38
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-14 17:33:42 -0400

    practice3

--- practice/js/prob3.js
+++ practice/js/prob3.js
@@ -1,29 +1,123 @@
-/**
- * Created by debbiewalker on 9/13/14.
- */
+//Created by debbiewalker on 9/13/14.
+
+
 
 //What will your movie ticket cost?
 
-//Prompt string statement "What is your age?"
-var number = prompt("What is your age?");
-//syntax: variablename = (conditional) ? value 1:value 2
-var discountAge = [x <= 10 === x >= 55]
+var under = 10;
+var over = 55;
+var between = 11-54;
+
+var regPriceAge = (number > 11 < 54);
+
+
+var number = prompt ("Please enter your age.");
+var discountAge = (number <= 10 || number >=55);
+if(discountAge){
+    console.log("You qualify for a discount; your reduced price is $7.00.")
+}else{
+    console.log("Your ticket price is $12.00.")
+}
+
+
+/*
+var under = <= 10;
+var over = (discountAge >= 55);
+var between = (11 >= 54);
+var discountAge = (under && over);
+
+var age = prompt ("Enter your age, please.");
+if(under || over){
+    console.log("You receive a discount, please $7.00. Enjoy your movie.")
+}else if(between){
+
+
+}
 
 
 
 
 
-//code: what age do you have to be to get a discount?
+
+//____________________________this code works, but is not correct________
+
+ //Prompt string statement "What is your age?"
+ var age = prompt("Enter your age.");
+ var matineePrice = 7;
+ var regPriceAge = (age > 11 || age < 54 );
+ if ( age <= 10 && age >= 55){
+ console.log("Your discount ticket = $7.00.")
+ }else if( age > 11 || age < 54 ){
+ console.log("Your ticket = $12.00.")
+ }else ( regPriceAge = matineePrice )
+ {
+ console.log("Unless it is between 2pm - 5pm," +
+ " then you receive a Matinee Price of $7.00.");
+ }
+ //_______________________________________________
+
+ if (time < 20) {
+ greeting = "Good day";
+ } else {
+ greeting = "Good evening";
+ }
+
+ if (time < 10) {
+ greeting = "Good morning";
+ } else if (time < 20) {
+ greeting = "Good day";
+ } else {
+ greeting = "Good evening";
+ }
+
+    + [age <= 10 || age >= 55]);
+//code: age conditionals
+var discountHours = "2 pm - 5 pm";
+var discountAge = [age <= 10 || age >= 55];
+var regularPrice = 12;
 
 
-var discountAge = [10 === 55]
-var regPriceAge = 11 - 54;
-//code: discountAge AND regularAge
+if (number = discountAge){
+    console.log("You qualify for a discount price of $7.00");
+}else (number != discountAge){
+    (regularPrice = discountAge){
+}if(discountHours){
+    console.log("Super Savor Discount = $7.00");
+}
+
+
+var discountAge = [];
+
+if (discountPrice = age <= 10) {
+}else (discountPrice = age >= 55);
+    console.log("You qualify for a discount price of $7.00.");
+}
 
 
 
 
-var discountAge = prompt("What is your age?");
+//what is the discount price?
+
+//what is the regular price?
+var regularPrice = "$12.00" 2 = 5
+
+
+
+var discountTime = ("12.00" 2 = 5)
+if(age 11 - 54){
+    //unless between 2 pm to 5 pm
+}else if(x {
+    //then the price = discount price
+}
+//code: discount age AND regular age
+if (age = number <= 10 || number >= 55){
+    console.log(discountPrice)
+}
+
+
+
+//syntax: variableName = (conditional) ? value 1:value 2
+//if user meets discountAge conditions, then cost = $7.00
 if (discountAge <= 10){
     //code if less than 10
 }else if (discountAge >= 55){
@@ -32,10 +126,26 @@ if (discountAge <= 10){
 
 
 
-var discountPrice = "$7.00";
+//code: what age do you have to be to get a discount?
+
+// Prompt string statement asking user what their age is...
+var enterAge = prompt("What is your age?");
+
+var discountAge = [age < 10 === age > 55]
+var regPriceAge = 11 - 54;
+//code: discountAge AND regularAge
+
+
+
+
+
+ var discountAge = [number <= 10 == number >= 55];
+
+
 
 //What age
 //How much is a regular price ticket?
 var regPrice = "$10.00";
 //What age pays regular price?
 var regPriceAge =
+*/
