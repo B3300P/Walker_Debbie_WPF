bee6f4fb408202166171feb63eb390fa9106143f
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-13 23:11:07 -0400

    practice

---
+++ practice/js/prob3.js
@@ -0,0 +1,41 @@
+/**
+ * Created by debbiewalker on 9/13/14.
+ */
+
+//What will your movie ticket cost?
+
+//Prompt string statement "What is your age?"
+var number = prompt("What is your age?");
+//syntax: variablename = (conditional) ? value 1:value 2
+var discountAge = [x <= 10 === x >= 55]
+
+
+
+
+
+//code: what age do you have to be to get a discount?
+
+
+var discountAge = [10 === 55]
+var regPriceAge = 11 - 54;
+//code: discountAge AND regularAge
+
+
+
+
+var discountAge = prompt("What is your age?");
+if (discountAge <= 10){
+    //code if less than 10
+}else if (discountAge >= 55){
+    //code if greater than 55
+}
+
+
+
+var discountPrice = "$7.00";
+
+//What age
+//How much is a regular price ticket?
+var regPrice = "$10.00";
+//What age pays regular price?
+var regPriceAge =
