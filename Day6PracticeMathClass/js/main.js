066ba8d06a82941cffb1d0819097a461c8af0e51
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-15 19:17:44 -0400

    FINALLY GOT IT!!!!!

--- Day6PracticeMathClass/js/main.js
+++ Day6PracticeMathClass/js/main.js
@@ -2,11 +2,18 @@
 - Lecture 00, Day 6 Practice Math Class */
 
 var min = (prompt("Please enter a min number."));
-var max = (prompt("Please enter a min number."));
+//you can add multiple brackets...
+var max = prompt("Please enter a min number.");
 
 //test to see that a number was actually entered!
 
 if(min===""){
     console.log("you did not enter number");
-    min = Number(prompt("Please reenter number"));
+    min = Number(prompt("Please re-enter number"));
+}
+
+//isNaN
+if(isNaN(min) || min === ""){
+    min = prompt("Re prompt, do not leave blank");
+    console.log ("Please re-enter number.");
 }
