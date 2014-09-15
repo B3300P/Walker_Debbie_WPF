0a0e5145c343e60a6aa07d64e8be563e04428725
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-15 14:52:29 -0400

    PROBLEM 1 SOLVED!!!!!!!!
    I was making this WAY TOO HARD

--- practice/js/prob1.js
+++ practice/js/prob1.js
@@ -4,25 +4,20 @@
 
 //Conditional Logic | Problem 1
 
-//Prompt user to input temperature...
-var number = prompt("What is the temperature?");
-//Give variable temperature an array for user to choose from...
-var temperature = ["Celsius", " Fahrenheit"];
-//Use math equations to convert Celsius into Fahrenheit...
-// (referenced :http://albireo.ch/temperatureconverter/formula.htm for the equations)
-var degreeC = (number-32) * 5/9; //if number was 32: (32-32) = 0 * 5/9 = 0
-//Declare what the formula is for Fahrenheit...
-var degreeF = number * 9/5 +32; //if number was 32: 32 *
-//Prompt user to choose a temperature...
-var temp = prompt("What temperature measurement would you prefer? " + temperature);
-//compare using logical operators; either (if) or (else if)...
-//if user chooses Celsius or c it will only choose Celsius from the temp prompt
-//which gives the user chooses Celsius from the array variable temperature
-if(temp == "Celsius" || temp == "c") {
-    //then print to the console what the degree (using math formula) is in Celsius
-    console.log ("The temperature is " + number + degreeC + " Celsius" )}
-//OR else if the user chooses Fahrenheit from the array variable temperature
-else if(temp == "Fahrenheit" || temp == "f"){
-    console.log("The temperature is " + number + degreeF + " Fahrenheit")
+
+//input: since there are two scales for temperature prompt user to choose c/Celsius or f/Fahrenheit
+var scale = prompt("Choose your conversion; c for Celsius or f for Fahrenheit.");
+//input: have user enter the temperature they want converted
+var number = prompt("Enter the temperature you want to convert.");
+//declare the math formula for Celsius and Fahrenheit (referenced http://albireo.ch/temperatureconverter/formula.htm)
+var c = (number-32) * 5/9; //Celsius math equation
+var f = number * 9/5 +32; //Fahrenheit math equation
+//if c == f then f == c
+if(c){
+    console.log(f + " Fahrenheit.");
 }
+else if(f){
+    console.log(c + " Celsius.");
+}
+
 
