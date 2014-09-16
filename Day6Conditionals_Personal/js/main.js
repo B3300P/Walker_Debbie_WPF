1928ff0d14994803c9822899e9f9ee496ff29d27
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-15 23:36:20 -0400

    Problem
    wip

--- Day6Conditionals_Personal/js/main.js
+++ Day6Conditionals_Personal/js/main.js
@@ -7,27 +7,23 @@
 //prompt user to enter mode of transportation
 // mph = r=d/t; referenced (http://www.math.com/school/subject1/lessons/S1U2L3DP.html)
 
-var carRate = 10;
 var carDistance = 10;
 var carTime = 10;
-var carMph = carRate == carDistance/carTime;
+var carRate = carDistance/carTime;
 
-var scooterMiles = 10;
 var scooterDistance = 10;
 var scooterTime = 10;
-var carMph = carRate == carDistance/carTime;
+var scooterRate = carDistance/carTime;
 
-var bikeMiles = 10;
 var bikeDistance = 10;
 var bikeTime = 10;
-var carMph = carRate == carDistance/carTime;
+var bikeRate = carDistance/carTime;
 
 //prompt user: choose a mode of transportation
 var mode = prompt("Choose a mode of transportation: car, scooter, bike.");
 //add if statement mode is nothing, re-prompt until a choice is made
-// (loop syntax: for
-
 if (mode === ""){
+    // (loop syntax: for
     for (i=1; i<=5 ; i++){
         var mode = prompt("Choose a mode of transportation: car, scooter, bike.");
         console.log("Please try again.");
@@ -35,6 +31,7 @@ if (mode === ""){
 }else{
     console.log("Great choice.");
 }
+
 
 
 
