9c1868a8c24b11d06af1bb7a19522dd0e1a81c4d
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-15 22:46:47 -0400

    Problem
    Equations

--- Day6Conditionals_Personal/js/main.js
+++ Day6Conditionals_Personal/js/main.js
@@ -2,4 +2,26 @@
  - Lecture 00, Day 6 Conditionals, Personal */
 
 
-//Problem: What is the 
+//Problem: Time Calculator
+
+//prompt user to enter mode of transportation
+
+var carRate = 10;
+var carDistance = 10;
+var carTime = 10;
+var carMph = carRate == carDistance/carTime;
+
+var scooterMiles = 10;
+var scooterDistance = 10;
+var scooterTime = 10;
+var carMph = carRate == carDistance/carTime;
+
+var bikeMiles = 10;
+var bikeDistance = 10;
+var bikeTime = 10;
+var carMph = carRate == carDistance/carTime;
+
+var mph = r=d/t;
+
+
+
