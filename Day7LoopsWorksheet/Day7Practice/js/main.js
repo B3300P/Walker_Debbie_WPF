96f8c72c77f5763bf0c46c508feaf14f2f5642c4
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-17 18:07:53 -0400

    wip

--- Day7LoopsWorksheet/Day7Practice/js/main.js
+++ Day7LoopsWorksheet/Day7Practice/js/main.js
@@ -1,10 +1,13 @@
 /*Debbie Walker, September 17, 2014, Web Programming Fundamentals
- - Lecture 00, Day 7 Conditionals, Loops*/
+ - Lecture 00, Day 7 Conditionals, Loops
 
 
 //Loop Practice
 
  //WE WANT THIS TO KEEP ASKING IN A LOOP (ADDED ON SEPT 17)while loop runs as long as condition is true
+
+//       NEED HELP TROUBLE SHOOTING!!!!!
+
 
 var min = (prompt("Please enter a min number."));
 //you can add multiple brackets...
@@ -23,8 +26,28 @@ if(isNaN(min) || min === ""){
     console.log ("Please re-enter number.");
 }
 
- while(isNan(min)) || min==="")
+ while(isNaN(min) || min==="")
 {
     //while loop runs as long as condition is true
 min = prompt("Please enter a number and do not leave blank.");
 }
+
+*/
+
+//__________________________
+
+//Basic While Loops
+
+//initializing he counter variable
+var counter = 0;
+
+while(counter<200){
+    //starting the while loop with condition
+    //while condition is true, loop will run
+    //when condition is false, loop stops
+    console.log("counter is counting!");
+    counter++;
+}
+
+
+
