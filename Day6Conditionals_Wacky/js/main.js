7add8f4efb6a26a07fabb9f0373d6b35ef04c916
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-17 16:52:07 -0400

    Problem
    wip

--- Day6Conditionals_Wacky/js/main.js
+++ Day6Conditionals_Wacky/js/main.js
@@ -1,2 +1,12 @@
 /*Debbie Walker, September 13, 2014, Web Programming Fundamentals
  - Lecture 00, Day 6 Conditionals, Industry */
+
+
+//area
+
+var h = 1;
+var l = 1;
+var w = 1;
+
+var area = (l x w x h);
+
