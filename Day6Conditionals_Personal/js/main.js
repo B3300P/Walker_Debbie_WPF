b239d83612388b41fc3d5e6ef4a1e38fa28b7144
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-18 00:22:51 -0400

    problem FINAL
    CREDITS GO OUT TO:
    Vid for logical flowcharting
    Jeff for getting the numbers to add up - I was REALLY
    confused!!!!

--- Day6Conditionals_Personal/js/main.js
+++ Day6Conditionals_Personal/js/main.js
@@ -5,56 +5,72 @@
 //Problem: Choose your ride.
 
 //alert user: welcome...
-alert("Welcome back to CHOOSE MY RIDE.");
+alert("Welcome back to" +
+    "                                                                   CHOOSE MY RIDE!");
 //alert user: how to play
 alert("Enter either car, scooter or bike for each question.");
-//ask weather question...
-
-var vehicle = ["bike", "scooter", "car"];
-var carSum = 1;
+//declare variables
+var vehicle = ["bike", " scooter", " car"];//array of choices
+var carSum = 1;//sum for each mode of transportation...
 var scooterSum = 1;
 var bikeSum = 1;
-
-var weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle);
-    if (vehicle[1]){
-        bikeSum++
-    } else if (vehicle[2]){
-        scooterSum++
-    }else if (vehicle[3]){
-        carSum++
-    }else{
+//if/then statements...
+//prompt user to choose a mode of transportation after each question...
+weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle);
+if (vehicle[0]){//if array: vehicle unit is [0] is true
+        bikeSum++; //then add bike sum
+} else if (vehicle[1]){// else if vehicle unit is [1]
+        scooterSum++; //then add scooter sum
+}else if (vehicle[2]){//else if...vehicle unit [2]
+        carSum++; //then add car sum
+}else{//then...re-prompt if no answer was given (this would have looped, if there was more time)
         weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle)
-    }
+}
 
-var weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle);
-if (vehicle[1]){
-    bikeSum++
-} else if (vehicle[2]){
-    scooterSum++
-}else if (vehicle[3]){
-    carSum++
+// /repeat comments for all questions...
+weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle);
+if (vehicle[0]){
+    bikeSum++;
+} else if (vehicle[1]){
+    scooterSum++;
+}else if (vehicle[2]){
+    carSum++;
 }else{
     weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle)
 }
 
-var weather = prompt("It's awfully windy out today, you choose... " + vehicle);
-if (vehicle[1]){
-    bikeSum++
-} else if (vehicle[2]){
-    scooterSum++
-}else if (vehicle[3]){
-    carSum++
+weather = prompt("It's awfully windy out today, you choose... " + vehicle);
+if (vehicle[0]){
+    bikeSum++;
+} else if (vehicle[1]){
+    scooterSum++;
+}else if (vehicle[2]){
+    carSum++;
 }else{
     weather = prompt("It's awfully windy out today, you choose... " + vehicle)
 }
 
-var weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle);
-if (vehicle[1]){
-    bikeSum++
-} else if (vehicle[2]){
-    scooterSum++
-}else if (vehicle[3]){
-    carSum++
+weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle);
+if (vehicle[0]){
+    bikeSum++;
+} else if (vehicle[1]){
+    scooterSum++;
+}else if (vehicle[2]){
+    carSum++;
 }else{
     weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle)
 }
+
+//associate sums to arrays for total points earned (this is where Jeff helped!!!!!!!
+// he really deserves extra credit!!!!)
+if ((carSum >= scooterSum) && (carSum >= bikeSum)){
+    var totalSum = vehicle[2];
+} else if ((scooterSum >= carSum) && ( scooterSum >= bikeSum )) {
+     totalSum = vehicle[1];
+} else if ((bikeSum >= scooterSum) && (bikeSum >= scooterSum)){
+    totalSum = vehicle[0];
+}
+
+alert("CONGRATULATIONS, your ride for today is a " + totalSum + ".");
+
+//CREDIT LINE: Vid helped me with the logic and Jeff helped me with the equation.
