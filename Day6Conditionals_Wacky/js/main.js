fa0d0137e56d6fe4a6f1ded59ca854745d25541e
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-17 23:08:48 -0400

    Problem 1

--- Day6Conditionals_Wacky/js/main.js
+++ Day6Conditionals_Wacky/js/main.js
@@ -2,29 +2,28 @@
  - Lecture 00, Day 6 Conditionals, Industry */
 
 
-//What size truck would you need to move two dressers?
+//Building a cat house.
 
-var
+//Ask user what the size of their cat is?
+// Cats need an extra square foot of space in their house
+// to be cozy.
 
 //Prompt user to input dimensions;
-var height = prompt("Enter your height.");
-    if(height === ""){
-    console.log("Please re-enter.");
-    }
-    else if (isNaN(height)){
-        console.log("Please re-enter.");
-    }
-var width = prompt("Enter your width.");
-    if(width === ""){
-    console.log("Please re-enter.");
-}
-var depth = prompt("Enter your depth.");
-    if(depth === ""){
-    console.log("Please re-enter.");
-}
-var area = (height * width * depth);
-    console.log("This is the volume of your box. " + area);
 
-}else if(isNaN(userId) != true){
-    //print not valid...
-    console.log("Not a valid userId.");
+var catHeight = prompt("What is your cats height?");
+    if (isNaN(catHeight)){
+        catHeight = prompt("What is your cats height?");
+    }
+var catLength = prompt("What is your cat length?");
+    if (isNaN(catLength)){
+        catLength = prompt("What is your cat length?");
+    }
+var catStature = prompt("How broad is your cat?");
+    if (isNaN(catStature)){
+        catStature = prompt("How broad is your cat?");
+    }
+var boxArea = (catHeight * catLength * catStature)+12;
+
+alert("Your Total = " + boxArea + ".");
+alert("Thank you.");
+
