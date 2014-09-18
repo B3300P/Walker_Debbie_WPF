ffeae07820d004ed2be25f4b0c938ea46fe20810
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-17 23:40:59 -0400

    problem
    wip

--- Day6Conditionals_Personal/js/main.js
+++ Day6Conditionals_Personal/js/main.js
@@ -2,75 +2,59 @@
  - Lecture 00, Day 6 Conditionals, Personal */
 
 
-//Problem: What mode of transportation should you choose for today?
-
-//consider a mileage calculator into questions??
-// mph = r=d/t; referenced (http://www.math.com/school/subject1/lessons/S1U2L3DP.html)
-//var scooterTime = 10;
-//var scooterRate = distance/scooterTime;
-//var bikeTime = 10;
-//var bikeRate = distance/bikeTime;
-//var distance = 10
-//_________________________
-
-//Car, scooter or bike?
-//Choose the best mode of transportation based on today's weather forecast? (enter weather link here)
-//Bike (1pt), Scooter (1pt), Car (0pt)
-//etc., Every time a variable is entered ++1 is added to that variable
-//Total points for best mode of transportation = the perfect vehicle for user
-//Conclusion = _____ is the best choice for you.
+//Problem: Choose your ride.
 
 //alert user: welcome...
-alert("Welcome back to CHOOSE MY RIDE.")
+alert("Welcome back to CHOOSE MY RIDE.");
 //alert user: how to play
-alert("Enter either car, scooter or bike for each question.")
+alert("Enter either car, scooter or bike for each question.");
 //ask weather question...
-var weather = prompt("The weather is clear and sunny, you choose...");
-var car = 1
-var scooter = 1
-var bike = 1
-var carSum = 1
-var scooterSum = 1
-var bikeSum = 1
 
+var vehicle = ["bike", "scooter", "car"];
+var carSum = 1;
+var scooterSum = 1;
+var bikeSum = 1;
 
+var weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle);
+    if (vehicle[1]){
+        bikeSum++
+    } else if (vehicle[2]){
+        scooterSum++
+    }else if (vehicle[3]){
+        carSum++
+    }else{
+        weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle)
+    }
 
+var weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle);
+if (vehicle[1]){
+    bikeSum++
+} else if (vehicle[2]){
+    scooterSum++
+}else if (vehicle[3]){
+    carSum++
+}else{
+    weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle)
+}
 
+var weather = prompt("It's awfully windy out today, you choose... " + vehicle);
+if (vehicle[1]){
+    bikeSum++
+} else if (vehicle[2]){
+    scooterSum++
+}else if (vehicle[3]){
+    carSum++
+}else{
+    weather = prompt("It's awfully windy out today, you choose... " + vehicle)
+}
 
-
-/*
-
-var points = 1
-var car =
-var scooter = points++;
-var bike = points++;
-var totalCar = car * car;
-
-alert(car);
-var total = prompt(car);
-
-var mode = ["scooter ", "car", " bike"];
-var answer = ["yes"];
-
-alert("Welcome back to"   +
-    "                                                 CHOOSE MY RIDE!");
-alert("Receive a point for each choice. " +
-    "                                                     The most points equal your ride for the day.");
-alert("Enter either car, scooter, or bike for each question.");
-var goodWeather = prompt("The temperature is between 75-90 and sunny, I choose...");
-var choose = prompt("Choose your " + mode + " of transportation.");
-    if(bike){
-    console.log("75 degrees is perfect weather for a " + bike + "ride!");
-        }else if(scooter){
-    console.log("The wind in your hair would feel great scooting around on a" + scooter + "today!");
-        }else if(car){
-
-        }
-
-
-
-
-/*if (car) {
-    console.log("copy here"
-});
-    */
+var weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle);
+if (vehicle[1]){
+    bikeSum++
+} else if (vehicle[2]){
+    scooterSum++
+}else if (vehicle[3]){
+    carSum++
+}else{
+    weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle)
+}
