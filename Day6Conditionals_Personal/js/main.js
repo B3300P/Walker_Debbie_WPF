f7a06dbf02e73c50e6f06fbcaf3c6a7eb6980f8c
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-18 00:50:09 -0400

    Vid troubleshoot FINAL
    added ====
    took spaces out of array

--- Day6Conditionals_Personal/js/main.js
+++ Day6Conditionals_Personal/js/main.js
@@ -10,18 +10,18 @@ alert("Welcome back to" +
 //alert user: how to play
 alert("Enter either car, scooter or bike for each question.");
 //declare variables
-var vehicle = ["bike", " scooter", " car"];//array of choices
+var vehicle = ["bike", "scooter", "car"];//array of choices
 var carSum = 1;//sum for each mode of transportation...
 var scooterSum = 1;
 var bikeSum = 1;
 //if/then statements...
 //prompt user to choose a mode of transportation after each question...
 weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle);
-if (vehicle[0]){//if array: vehicle unit is [0] is true
+if (weather === vehicle[0]){//if array: vehicle unit is [0] is true
         bikeSum++; //then add bike sum
-} else if (vehicle[1]){// else if vehicle unit is [1]
+} else if (weather === vehicle[1]){// else if vehicle unit is [1]
         scooterSum++; //then add scooter sum
-}else if (vehicle[2]){//else if...vehicle unit [2]
+}else if (weather === vehicle[2]){//else if...vehicle unit [2]
         carSum++; //then add car sum
 }else{//then...re-prompt if no answer was given (this would have looped, if there was more time)
         weather = prompt("The weather is clear in sunny Florida, you choose... " + vehicle)
@@ -29,33 +29,33 @@ if (vehicle[0]){//if array: vehicle unit is [0] is true
 
 // /repeat comments for all questions...
 weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle);
-if (vehicle[0]){
+if (weather === vehicle[0]){
     bikeSum++;
-} else if (vehicle[1]){
+} else if (weather === vehicle[1]){
     scooterSum++;
-}else if (vehicle[2]){
+}else if (weather === vehicle[2]){
     carSum++;
-}else{
+}else {
     weather = prompt("The weather just took a turn for the worse, you choose... " + vehicle)
 }
 
 weather = prompt("It's awfully windy out today, you choose... " + vehicle);
-if (vehicle[0]){
+if (weather === vehicle[0]){
     bikeSum++;
-} else if (vehicle[1]){
+} else if (weather === vehicle[1]){
     scooterSum++;
-}else if (vehicle[2]){
+}else if (weather === vehicle[2]){
     carSum++;
 }else{
     weather = prompt("It's awfully windy out today, you choose... " + vehicle)
 }
 
 weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle);
-if (vehicle[0]){
+if (weather === vehicle[0]){
     bikeSum++;
-} else if (vehicle[1]){
+} else if (weather === vehicle[1]){
     scooterSum++;
-}else if (vehicle[2]){
+}else if (weather === vehicle[2]){
     carSum++;
 }else{
     weather = prompt("-40 degrees in miserable Minnesota, you choose... " + vehicle)
@@ -70,6 +70,10 @@ if ((carSum >= scooterSum) && (carSum >= bikeSum)){
 } else if ((bikeSum >= scooterSum) && (bikeSum >= scooterSum)){
     totalSum = vehicle[0];
 }
+
+console.log(carSum);
+console.log(scooterSum);
+console.log(bikeSum);
 
 alert("CONGRATULATIONS, your ride for today is a " + totalSum + ".");
 
