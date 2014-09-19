f40d095e81770043d4e89f826759865fe96161a4
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 16:39:55 -0400

    file trouble shooting

--- Day7Loops_Worksheet/Day7PracticeWorksheet/js/forLoop.js
+++ Day7Loops_Worksheet/Day7PracticeWorksheet/js/forLoop.js
@@ -3,16 +3,16 @@
 
 //For Loop Practice
 
-/*
 
 var nb = 0;
-var array = ["s", "m", "l", "xl"];
+var s = 1-6;
+var m = 7-1;
+var l = 13-18;
+var array = ["nb", "s", "m", "l"];
 
-var shoe = (prompt("Please enter your child's shoe size."));
-console.log("Hi");
-
-
-if(shoe < 1 == nb || shoe > nb == array[1]){
+var shoe = prompt("Please enter your child's shoe size." + array);
+    console.log(array);
+if(shoe < 1 == nb || shoe > nb == array){
     console.log("Shoe size is" + array);
 }
 
@@ -32,6 +32,8 @@ while(isNaN(min) || min==="")
     //while loop runs as long as condition is true
     min = prompt("Please enter a number and do not leave blank.");
 }
-*/
 
-alert("test")
+
+alert("test");
+
+    */
