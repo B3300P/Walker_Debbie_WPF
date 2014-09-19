1b4bebb1f2f9ace7ffce96f130d288ca42748e70
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 16:25:57 -0400

    file set up - refig

--- Day7Loops_Worksheet/Day7PracticeWorksheet/js/forLoop.js
+++ Day7Loops_Worksheet/Day7PracticeWorksheet/js/forLoop.js
@@ -1,17 +1,20 @@
-/*Debbie Walker, September 17, 2014, Web Programming Fundamentals
- - Lecture 00, Day 7 Conditionals, For Loops
-*/
+//Debbie Walker, September 17, 2014, Web Programming Fundamentals
+//- Lecture 00, Day 7 Conditionals, For Loops
 
 //For Loop Practice
 
+/*
 
 var nb = 0;
 var array = ["s", "m", "l", "xl"];
-var shoe = prompt("Please enter your child's shoe size.");
-if(shoe < 1 == nb || shoe > nb == array[1]);
-console.log("Shoe size is" array)
+
+var shoe = (prompt("Please enter your child's shoe size."));
+console.log("Hi");
 
 
+if(shoe < 1 == nb || shoe > nb == array[1]){
+    console.log("Shoe size is" + array);
+}
 
 if(min===""){
     console.log("you did not enter number");
@@ -29,3 +32,6 @@ while(isNaN(min) || min==="")
     //while loop runs as long as condition is true
     min = prompt("Please enter a number and do not leave blank.");
 }
+*/
+
+alert("test")
