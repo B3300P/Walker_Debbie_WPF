d1c77f6fc3e1d221603eb2c0f7eba0e8037e666a
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 17:46:01 -0400

    function practice

---
+++ Day8Functions_Worksheet/js/functions.js
@@ -0,0 +1,17 @@
+//Debbie Walker, September 19, 2014, Web Programming Fundamentals
+//- Lecture 00, Day 7 Functions
+
+//Function Practice
+
+var fruit = ["grape","pear", "apple", "bananas"];
+//loop through to check for unit in array:
+
+var totalFruit = 0;
+
+if(fruit[0] === "grape"){
+    totalFruit++;
+}
+    console.log(totalFruit);
+
+//++ makes te totalFruit variable add 1 unit so that it will loop through by checking
+//to see if it equals anything in the array.
