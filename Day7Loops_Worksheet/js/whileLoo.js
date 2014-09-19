954bbda8f130d3265f789b5f81758f909165dabb
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 16:50:55 -0400

    while loop set up

--- Day7Loops_Worksheet/js/whileLoo.js
+++ Day7Loops_Worksheet/js/whileLoo.js
@@ -1,3 +1,4 @@
-/**
- * Created by debbiewalker on 9/19/14.
- */
+//Debbie Walker, September 19, 2014, Web Programming Fundamentals
+//- Lecture 00, Day 7 Conditionals, While Loops
+
+//For While Practice
