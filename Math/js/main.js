8802002ccbd02d6d62d6c2311abb845b63665081
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-20 00:57:32 -0400

    functions

--- Math/js/main.js
+++ Math/js/main.js
@@ -2,53 +2,42 @@
  - Lecture 00, Day 9 Functions */
 
 
-//Parameters for function: Calculate the circumference of a circle.
-//naming/calling the function
+//Calculate the circumference of a circle.
+
+//parameters for function...
 var result = circumference();
 
-//creating function
+//declaring function...
 function circumference(){
     var radius = prompt("Enter the radius of the circle","");
         if(radius===""){
              console.log("You did not enter a number.");
             radius = Number(prompt("Please re-enter the number."));
-
     }
     var circle = 2 * Math.PI * radius;
-    return ("The circumference is " + Math.ceil(circle) + ".");
-
+    return ("The circumference is " + Math.round(circle) + ".");
     }
-
 console.log(result);
 
 
-/*
+//Stung!
 
-//when you call it can also be a variable like://to get user to input (prompt), but make sure
-//to validate the prompt when there is no answer
-var results = learning();
+//given: parameters for function = user input
+var beeStings = 8.666666667;
+var weight = 0;
+var stings = killed();
 
-//"leaning()" is what I would do to call the functions (kind of like a variable, it's an
-//action MACHINE (container that can do stuff - functions).
-learning();//calling function is getting stuff out of the function (this would be me calling
-//the ice cream factory for products.
-
-
-//create the name of the function: the function is the ice cream factory
-//(t) = parameter for input (this is the argument; so for an area there would be two parameter
-//whereas a dog's age would only be one parameter
-function learning(t){
-    //between curly brackets is the work being done
-    //note from boss to workers to output this product
-    var rules = ("Follow the instructions.");
-    var practice = (" Or else you'll flunk.");
-    var success = rules + practice;
-    //tell the workers to return success
-    return success;
-
+//declaring function...
+function killed(){
+    var weight = prompt("Enter your weight here","");
+    if(weight===""){
+        console.log("Sorry I didn't catch your weight.");
+        weight = Number(prompt("Please re-enter the number."));
+    }
+    var math = beeStings * weight;
+    return ("It would take " + Math.round(math) + " bee stings per pound to kill an animal based on that weight.");
 }
+console.log(stings);
 
-//console results
-console.log(results);
 
-*/
+
