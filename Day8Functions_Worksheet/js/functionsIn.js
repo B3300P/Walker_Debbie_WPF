7fb39866df9dba2456996668c6e8d3a6bdb3fda0
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 20:48:46 -0400

    practice

--- Day8Functions_Worksheet/js/functionsIn.js
+++ Day8Functions_Worksheet/js/functionsIn.js
@@ -5,7 +5,7 @@
 //arguments & parameters
 //arguments - goes into functions
 //parameters - catches the argument in the function
-
+/*
 
 //Allows us to make our code more flexible!
 
@@ -28,7 +28,7 @@ calcArea(width,height);
 var funcResult = calcArea(20,20);
 
 //run the function again
-/*calcArea(10,10);*/
+/*calcArea(10,10);
 
 
 //this adds returns
@@ -46,6 +46,7 @@ calcAge(yearsOld * dogAge);
     console.log(calcAge(15,));
 
 }
+
 
 //_______________________
 
@@ -69,3 +70,34 @@ calcAge(yearsOld * dogAge);
     //logging to console
 
 
+*/
+
+
+
+
+
+//when you call it can also be a variable like://to get user to input (prompt)
+var results = learning();
+
+//"leaning()" is what I would do to call the functions (kind of like a variable, it's an
+//action MACHINE (container that can do stuff - functions).
+learning();//calling function is getting stuff out of the function (this would be me calling
+//the ice cream factory for products.
+
+
+//create the name of the function: the function is the ice cream factory
+//(t) = parameter for input (this is the argument; so for an area there would be two parameter
+//whereas a dog's age would only be one parameter
+function learning(t){
+    //between curly brackets is the work being done
+    //note from boss to workers to output this product
+    var rules = ("Follow the instructions.");
+    var practice = (" Or else you'll flunk.");
+    var success = rules + practice;
+    //tell the workers to return success
+    return success;
+
+}
+
+//console results
+console.log(results);
