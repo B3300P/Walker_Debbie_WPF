2433dd43cc78098593d939b407175027da05fda1
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-19 22:28:26 -0400

    UGH file naming!!!!

---
+++ Day8Functions_Worksheet/js/main.js
@@ -0,0 +1,61 @@
+/*Debbie Walker, September 13, 2014, Web Programming Fundamentals
+ - Lecture 00, Day 9 Functions */
+
+
+//Function Worksheet
+
+
+//1.calculate the are of a rectangle:
+var width = 5;//variables given
+var height = 6;
+//call the function and pass in arguments
+calcArea(width,height);
+
+//create function and pass arguments into parameters.
+
+function calcArea (w,h){
+    var area = w*h;
+    console.log(area);
+
+    return area;
+}
+//make sure these are outside the function
+
+
+var funcResult = calcArea(20,20);
+
+//run the function again
+calcArea(10,10);
+
+
+
+
+//1. Calculate the circumference of a circle.
+//Parameters for function:
+
+var radius = prompt("Enter the radius of the circle","");
+var circle = 2 * Math.PI * radius;
+alert("The circumference is "+circle+".")
+}
+
+
+
+//2. variable for radius
+
+
+//3. creating function
+
+
+//FUNCTION CALCULATION
+
+
+//using the formula
+//xxxxx
+
+//returning value
+//return XXXX
+
+//4. calling function
+//var result =
+
+//logging to console
