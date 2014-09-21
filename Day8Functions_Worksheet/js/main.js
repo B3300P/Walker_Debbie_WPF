a49a5475882ba8823aeff8354cb7182634c48841
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-21 19:30:48 -0400

    syntax for functions

--- Day8Functions_Worksheet/js/main.js
+++ Day8Functions_Worksheet/js/main.js
@@ -1,3 +1,28 @@
 /*
  Debbie Walker, September 17, 2014, Web Programming Fundamentals - Lecture 00, Day 8 Functions
  */
+
+//3 types of functions=["Basic functions", "Procedures", "Anonymous"]
+//syntax for Basic
+// var function =functionName(){ //INVOKING is the input that calls the function//it can contains arguments, like what color
+//code the function runs; //it can contain parameters, like buckets for arguments
+//results
+// {
+//console.log(functions) //calls a return from the var functions
+
+
+//syntax for Procedure
+// function functionName(){ INVOKING is the input that calls the function//it can contains arguments, like what color
+//code the function;
+//return
+// {
+// console.log(functionName) //Procedures do not return a value!!!
+
+
+//syntax for Anonymous
+// function functionName(){ INVOKING is the input that calls the function//it can contains arguments, like what color
+//code the function runs;
+//return
+// {
+//var functionName(); INVOKING (input data)
+// console.log(functionName) //prints return
