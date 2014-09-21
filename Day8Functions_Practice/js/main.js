7dc6578fb3c46ec0cfb7ba99733575790e998443
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-21 16:56:40 -0400

    example 2

--- Day8Functions_Practice/js/main.js
+++ Day8Functions_Practice/js/main.js
@@ -54,3 +54,25 @@ if(fruit[0] === "grape"){
 //to see if it equals anything in the array.
 }
 console.log(totalFruit);
+
+
+//Basic For Loop
+//for syntax definition (initialization, condition, increment of change)
+
+//INITIALIZATION
+//i=0, this means that initialization equals index 0 in the array.
+//CONDITION
+//i<fruit.length, fruit.length is a method that gives the array fruit the container
+//counts (which are variables and equations)
+//INCREMENT OF CHANGE
+//i++, is the increment that you want the loop to check. So i++ would be what ever i=#
+//plus 1 each time
+
+//this is the syntax for it
+for(var i=0; i<fruit.length ; i++){
+    //for loop initializes the variable i, sets a conditions
+    //i is less than 20, and adds increments of change + 4
+    console.log("i = " + fruit[i]);
+    //show the value of i
+    //runs as long as i is less than 20
+}
