457a5a1f48c90d70d4118d791c56f22e7570d8d4
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-21 16:32:11 -0400

    example 1

--- Day8Functions_Practice/js/main.js
+++ Day8Functions_Practice/js/main.js
@@ -13,3 +13,19 @@
 
 
 //Allows us to make our code more flexible!
+
+
+var width = 10;
+var height = 10;
+//call the function and pass in arguments
+calcArea(width,height);
+
+//create function and pass arguments into parameters.
+
+function calcArea (w,h){
+    var area = w*h;
+    console.log(area);
+
+    return area;
+}
+//make sure these are outside the function
