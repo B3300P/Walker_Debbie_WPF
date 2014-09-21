d8874e9266e490ef7b10aab21d7cf90e0c14d5ad
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-21 17:49:32 -0400

    syntax

--- Day8Functions_Worksheet/js/functions.js
+++ Day8Functions_Worksheet/js/functions.js
@@ -1,2 +1,30 @@
 /*Debbie Walker, September 19, 2014, Web Programming Fundamentals
  - Lecture 00, Day 9 Functions */
+
+
+//3 types of functions=["Basic functions", "Procedures", "Anonymous"]
+//syntax for Basic
+        // var function =functionName(){ //INVOKING is the input that calls the function//it can contains arguments, like what color
+                //code the function runs; //it can contain parameters, like buckets for arguments
+                //results
+        // {
+        //console.log(functions) //calls a return from the var functions
+
+
+//syntax for Procedure
+        // function functionName(){ INVOKING is the input that calls the function//it can contains arguments, like what color
+                //code the function;
+                //return
+        // {
+        // console.log(functionName) //Procedures do not return a value!!!
+
+
+//syntax for Anonymous
+        // function functionName(){ INVOKING is the input that calls the function//it can contains arguments, like what color
+                //code the function runs;
+                //return
+        // {
+        //var functionName(); INVOKING (input data)
+        // console.log(functionName) //prints return
+
+//Problem 1. Circumference
