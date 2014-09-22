bf3220a291999a81fcc00dc37a0ba810de851a65
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-22 17:58:11 -0400

    practice 1

--- Day9Function_Practice/js/main.js
+++ Day9Function_Practice/js/main.js
@@ -2,3 +2,31 @@
  * Created by debbiewalker on 9/22/14.
  */
 
+
+/**
+ * Created by debbiewalker on 9/22/14.
+ */
+
+
+//random number function
+//loop
+
+var min = 10;
+var max = 100;
+
+var ranNum=randomizer(min,max);
+console.log(ranNum);
+
+function randomizer(mn,mx){
+
+    var randomNumber = Math.round(Math.random() * (mx-mn)+mn);//math to round up, math random parameters
+    return randomNumber;
+
+
+}
+
+for(var i = 0; i<5; i++){
+    console.log(randomizer(min,max));
+
+
+}
