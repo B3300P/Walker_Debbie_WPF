d692dec05ece9c4fa485fd506300e4a87e5759f8
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-22 18:03:21 -0400

    practice 1 functions vs procedures

--- Day9Function_Practice/js/main.js
+++ Day9Function_Practice/js/main.js
@@ -11,13 +11,13 @@
 //random number function
 //loop
 
-var min = 10;
-var max = 100;
+var min = 10;//variable minimum number
+var max = 100;//variable maximum number
 
-var ranNum=randomizer(min,max);
-console.log(ranNum);
+var ranNum=randomizer(min,max);//variable random number
+console.log(ranNum);//print
 
-function randomizer(mn,mx){
+function randomizer(mn,mx){//function
 
     var randomNumber = Math.round(Math.random() * (mx-mn)+mn);//math to round up, math random parameters
     return randomNumber;
@@ -30,3 +30,20 @@ for(var i = 0; i<5; i++){
 
 
 }
+
+//This is a Function!
+function calcAreaF(width, height){
+
+    var area = width * height;
+    return area;
+
+}
+
+//This is a Procedure!
+//Just a list of steps to do
+function calcAreaP(widthdh, height){
+
+    var area = width * height;
+    console.log(area);
+}
+
