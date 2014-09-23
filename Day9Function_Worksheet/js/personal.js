5a8ba322cf1b8f71e3833cfbe830308af60b5df4
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-22 23:25:15 -0400

    wip

--- Day9Function_Worksheet/js/personal.js
+++ Day9Function_Worksheet/js/personal.js
@@ -5,6 +5,15 @@
 
     //1 serving size = x
 /*
+ Write down the width, length and height of your square or rectangular cake if you are unable to find the number easily on a serving chart or by using the serving calculator. Use a handheld or computer-based calculator and enter the width. Multiply the width by the length of your cake. Multiply that total by the height of your cake. The number you get will be the cubic inches of your cake. Divide that number by 8, since each serving will total 8 cubic inches, and the final number will reflect the number of servings your cake will yield.
+
+ Read more : http://www.ehow.com/how_2268970_calculate-wedding-cake-servings.html
+
+
+
+
+
+
 A ternary
 An else if
     An anonymous function
@@ -16,25 +25,27 @@ A function with three parameters.
 
 
 //2. create variables for the function
-var width=7;
-var length=12;
-var height=10;
-var volume = (width*length*height);
+var width = 7;
+var length = 30;
+var height = 20;
+
+
+//1.name the function
+function cakeVol(w,l,h){   //4.parameters
+    var volume = (w*l*h);  //5. add variable code
+    return volume;         //6. call the function to do the action
+
+}
 
 var cake = cakeVol=(width, length, height);//3. add arguments
+console.log("Based on the cake);
 
-console.log(cake);
+
+if(cake <= 10 === "serving 6")
 
 //var serving=1;
 //var array();
 
-//1.name the function
-function cakeVol(w,l,h){   //4.it does not matter what you call the argument here
-    volume = (w*l*h);  //5. add variable
-    return volume;         //6. call the function to do the action
-    console.log("Base on the ")
-
-}
 
 
 
