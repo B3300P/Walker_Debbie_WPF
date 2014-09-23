3fb0e15baa0d573b819910f31527861c48a52e3c
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-22 22:58:04 -0400

    industry idea

--- Day9Function_Worksheet/js/industry.js
+++ Day9Function_Worksheet/js/industry.js
@@ -1,3 +1,18 @@
 /**
  * Created by debbiewalker on 9/22/14.
  */
+
+Christmas Stockings
+
+//3ft = 1 yard, making stockings for everyone in the family (6 total),
+//how many yards will be needed
+//stockings are X x X.
+//Most fabrics are about 40 to 44 inches wide
+//A yard is usually 36 inches, or 3 feet
+//The amount of yardage on the bolt varies.
+// From the manufacturer it is generally around 50 to 100 yards
+
+
+var foot = 12
+var yard = 3
+var bolt =40
