ccebc7d9acde985236873bcd1a199308455a52c1
B3300P <debbiewalker@Debbies-MacBook-Pro.local>
2014-09-22 23:09:34 -0400

    prob set up

--- Day9Function_Worksheet/js/personal.js
+++ Day9Function_Worksheet/js/personal.js
@@ -19,20 +19,24 @@ A function with three parameters.
 var width=7;
 var length=12;
 var height=10;
+var volume = (width*length*height);
 
 var cake = cakeVol=(width, length, height);//3. add arguments
 
 console.log(cake);
 
 //var serving=1;
-
+//var array();
 
 //1.name the function
 function cakeVol(w,l,h){   //4.it does not matter what you call the argument here
-    var volume = (w*l*h);  //5. add variable
+    volume = (w*l*h);  //5. add variable
     return volume;         //6. call the function to do the action
+    console.log("Base on the ")
 
 }
+
+
 
 
 
